6785dcfba1d4700adbd737ff7036ab52c8de48e1
Chang <chang.rajani@gmail.com>
2016-12-14 16:31:51 +0200

    add register path

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/train', to: 'classify#train', as: 'train'
   post '/classify', to: 'classify#classify', as: 'classify'
+  get '/register', to: 'users#register', as: 'register'
 end
